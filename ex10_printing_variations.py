3595c2ec169be2ad0a2a1e4cacc74188c5dc3ac7
jbarnett1993 <jamesbarnett1993@icloud.com>
2020-06-08 12:32:57 +0100

    adding ex5-10

---
+++ ex10_printing_variations.py
@@ -0,0 +1,16 @@
+tabby_cat = "\I'm tabbed"
+persian_cat = "I'm split \non a new line"
+backslash_cat = "I'm \\ a \\ cat" 
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* fishies 
+\t* catnip\n\t* Grass
+"""
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+
---
+++ ex9_printing_printing_printing.py
@@ -0,0 +1,14 @@
+days = "Mon Tues Wed Thurs Fri"
+months = "Jan\nFeb\nMar\nApr\nMay\nJune\nJuly\nAug"
+
+
+print("These are the days", days)
+print("and these are the months", months)
+
+print("""
+There's something going on here,
+with the three double quotes,
+we'll be able to type as much as we lke.
+even 4 liness if we want""")
+
+
