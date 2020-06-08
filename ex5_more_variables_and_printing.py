166dd50f2e8fea2865da9f86a3d9655ab8d1959c
jbarnett1993 <jamesbarnett1993@icloud.com>
2020-06-08 10:50:06 +0100

    added ex 4 & 5

---
+++ ex5_more_variables_and_printing.py
@@ -0,0 +1,26 @@
+my_name = "James Barnett"
+my_age = 26 
+my_height = 190
+my_eyes = "Blue"
+my_teeth = "White"
+my_hair = "Brown"
+my_weight = 90
+
+
+print(f"let's talk about {my_name}")
+print(f"He's {my_height} inches tall")
+print(f"He's {my_weight} kilos heavy")
+print(f"Actually thats not so heavy")
+print(f"He's got {my_eyes} eyes and {my_hair} hair")
+print(f"His teeth are usually {my_teeth} unless he's had coffee in with case they're brown")
+
+
+
+
+#This line is tricky, try get it exactly right
+
+total = my_age + my_height + my_weight
+
+print(f"If i add {my_age}, {my_height} and {my_weight} i get {total}")
+
+
