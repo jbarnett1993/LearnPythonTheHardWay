9b54e89cdaf37595eb982c701f4dcb5dbf867ba5
jbarnett1993 <jamesbarnett1993@icloud.com>
2021-12-21 14:17:45 +0000

    Adding changes

--- ex19_functions_and_variables.py
+++ ex19_functions_and_variables.py
@@ -1,6 +1,6 @@
 def cheese_and_crackers(cheese_count_per_box, boxes_of_crackers):
    print(f"you have {cheese_count_per_box} cheeses") 
-   print(f"you have {boxes_of_crackers} boxes of crackerts")
+   print(f"you have {boxes_of_crackers} boxes of crackers")
    print(f"Thats a total of {cheese_count_per_box * boxes_of_crackers}")
 
 
--- ex20_functions_and_files.py
+++ ex20_functions_and_files.py
@@ -1,5 +1,5 @@
 from sys import argv
-input_file = argv
+script, input_file = argv
 
 def print_all(f):
     print(f.read())
